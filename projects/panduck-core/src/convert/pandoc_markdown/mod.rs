e3fc8c85962f5095a05827f1fce29784562b4091
Aster <galaster@foxmail.com>
2017-04-12 22:45:19 +0800

    🔮 Initialize support for pandoc markdown

--- projects/panduck-core/src/convert/pandoc_markdown/mod.rs
+++ projects/panduck-core/src/convert/pandoc_markdown/mod.rs
@@ -1,1 +1,128 @@
+use crate::ToNotedown;
+use notedown_ast::{ASTKind, ASTNode, ASTNodes};
+use pandoc_ast::{Block, Inline, MathType};
 
+impl ToNotedown for Block {
+    fn into_notedown(self) -> ASTNode {
+        match self {
+            Self::Plain(v) => {
+                unimplemented!()
+            }
+            Self::Para(v) => {
+                unimplemented!()
+            }
+            Self::LineBlock(_) => {
+                unimplemented!()
+            }
+            Self::CodeBlock(_, _) => {
+                unimplemented!()
+            }
+            Self::RawBlock(_, _) => {
+                unimplemented!()
+            }
+            Self::BlockQuote(_) => {
+                unimplemented!()
+            }
+            Self::OrderedList(_, _) => {
+                unimplemented!()
+            }
+            Self::BulletList(_) => {
+                unimplemented!()
+            }
+            Self::DefinitionList(_) => {
+                unimplemented!()
+            }
+            Self::Header(_, _, _) => {
+                unimplemented!()
+            }
+            Self::HorizontalRule => ASTKind::hr(None),
+            Self::Table(_, _, _, _, _, _) => {
+                unimplemented!()
+            }
+            Self::Div(_, _) => {
+                unimplemented!()
+            }
+            Self::Null => {
+                unimplemented!()
+            }
+        }
+    }
+}
+
+impl ToNotedown for Vec<Inline> {
+    fn into_notedown(self) -> ASTNode {
+        ASTKind::statements(self.into_notedown_list(), None)
+    }
+
+    fn into_notedown_list(self) -> ASTNodes {
+        self.into_iter().map(|f| f.into_notedown()).collect()
+    }
+}
+
+impl ToNotedown for Inline {
+    fn into_notedown(self) -> ASTNode {
+        match self {
+            Inline::Str(_) => {
+                unimplemented!()
+            }
+            Inline::Emph(_) => {
+                unimplemented!()
+            }
+            Inline::Underline(v) => {
+                unimplemented!()
+            }
+            Inline::Strong(v) => {
+                unimplemented!()
+            }
+            Inline::Strikeout(_) => {
+                unimplemented!()
+            }
+            Inline::Superscript(_) => {
+                unimplemented!()
+            }
+            Inline::Subscript(_) => {
+                unimplemented!()
+            }
+            Inline::SmallCaps(_) => {
+                unimplemented!()
+            }
+            Inline::Quoted(_, _) => {
+                unimplemented!()
+            }
+            Inline::Cite(_, _) => {
+                unimplemented!()
+            }
+            Inline::Code(_, _) => {
+                unimplemented!()
+            }
+            Inline::Space => {
+                unimplemented!()
+            }
+            Inline::SoftBreak => ASTKind::soft_break(None),
+            Inline::LineBreak => ASTKind::hard_break(None),
+            Inline::Math(m, t) => match m {
+                MathType::DisplayMath => {
+                    unimplemented!()
+                }
+                MathType::InlineMath => {
+                    unimplemented!()
+                }
+            },
+            Inline::RawInline(_, _) => {
+                unimplemented!()
+            }
+            Inline::Link(_, _, _) => {
+                unimplemented!()
+            }
+            Inline::Image(_, _, _) => {
+                unimplemented!()
+            }
+            Inline::Note(_) => {
+                unimplemented!()
+            }
+            Inline::Span(_, _) => {
+                unimplemented!()
+            }
+        }
+    }
+}
--- projects/panduck-core/src/convert/wiki/mod.rs
+++ projects/panduck-core/src/convert/wiki/mod.rs
@@ -1,1 +1,108 @@
+use crate::ToNotedown;
+use notedown_ast::ASTNode;
+use parse_wiki_text::{Configuration, Node};
 
+fn try_parse() {
+    let wiki_text = concat!("==Our values==\n", "*Correctness\n", "*Speed\n", "*Ergonomics");
+    let result = Configuration::default().parse(wiki_text);
+    assert!(result.warnings.is_empty());
+    for node in result.nodes {
+        if let Node::UnorderedList { items, .. } = node {
+            println!("Our values are:");
+            for item in items {
+                println!(
+                    "- {}",
+                    item.nodes
+                        .iter()
+                        .map(|node| match node {
+                            Node::Text { value, .. } => value,
+                            _ => "",
+                        })
+                        .collect::<String>()
+                );
+            }
+        }
+    }
+}
+
+impl ToNotedown for Node {
+    fn into_notedown(self) -> ASTNode {
+        match self {
+            Node::Bold { .. } => {
+                unimplemented!()
+            }
+            Node::BoldItalic { .. } => {
+                unimplemented!()
+            }
+            Node::Category { .. } => {
+                unimplemented!()
+            }
+            Node::CharacterEntity { .. } => {
+                unimplemented!()
+            }
+            Node::Comment { .. } => {
+                unimplemented!()
+            }
+            Node::DefinitionList { .. } => {
+                unimplemented!()
+            }
+            Node::EndTag { .. } => {
+                unimplemented!()
+            }
+            Node::ExternalLink { .. } => {
+                unimplemented!()
+            }
+            Node::Heading { .. } => {
+                unimplemented!()
+            }
+            Node::HorizontalDivider { .. } => {
+                unimplemented!()
+            }
+            Node::Image { .. } => {
+                unimplemented!()
+            }
+            Node::Italic { .. } => {
+                unimplemented!()
+            }
+            Node::Link { .. } => {
+                unimplemented!()
+            }
+            Node::MagicWord { .. } => {
+                unimplemented!()
+            }
+            Node::OrderedList { .. } => {
+                unimplemented!()
+            }
+            Node::ParagraphBreak { .. } => {
+                unimplemented!()
+            }
+            Node::Parameter { .. } => {
+                unimplemented!()
+            }
+            Node::Preformatted { .. } => {
+                unimplemented!()
+            }
+            Node::Redirect { .. } => {
+                unimplemented!()
+            }
+            Node::StartTag { .. } => {
+                unimplemented!()
+            }
+            Node::Table { .. } => {
+                unimplemented!()
+            }
+            Node::Tag { .. } => {
+                unimplemented!()
+            }
+            Node::Template { .. } => {
+                unimplemented!()
+            }
+            Node::Text { .. } => {
+                unimplemented!()
+            }
+            Node::UnorderedList { .. } => {
+                unimplemented!()
+            }
+        }
+    }
+}
