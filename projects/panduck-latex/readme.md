0f1b1e5cf1e7945c77340dc367b642e8e63687d9
Aster <galaster@foxmail.com>
2017-06-01 12:58:33 +0000

    🔮 Notedown -> LaTeX, `Emphasis`, `Strong`

--- projects/panduck-latex/readme.md
+++ projects/panduck-latex/readme.md
@@ -5,3 +5,5 @@ https://github.com/H2CO3/mdccc/blob/master/src/latex.rs
 
 https://github.com/lbeckman314/md2tex/blob/master/src/lib.rs
 
+https://www.overleaf.com/learn/latex/Bold%2C_italics_and_underlining#Italicized_text
+
--- projects/panduck-latex/src/traits/text/mod.rs
+++ projects/panduck-latex/src/traits/text/mod.rs
@@ -1,15 +1,90 @@
 use super::*;
 
 impl IntoLaTeX for TextSpan {
-    fn into_latex(self, cfg: &LaTeXConfig, ctx: &mut LaTeXContext) -> RcDoc<()> {
-        let _ = (cfg, ctx);
-        todo!()
+    fn into_latex(self, _: &LaTeXConfig, _: &mut LaTeXContext) -> RcDoc<()> {
+        match self {
+            TextSpan::Empty => RcDoc::text(""),
+            TextSpan::Normal(_) => {
+                unimplemented!()
+            }
+            TextSpan::Raw(_) => {
+                unimplemented!()
+            }
+            TextSpan::HTMLRawInline(_) => {
+                unimplemented!()
+            }
+            TextSpan::Emoji(_) => {
+                unimplemented!()
+            }
+            TextSpan::Escaped(_) => {
+                unimplemented!()
+            }
+            TextSpan::SoftNewline => {
+                unimplemented!()
+            }
+            TextSpan::HardNewline => {
+                unimplemented!()
+            }
+            TextSpan::CheckBox(_) => {
+                unimplemented!()
+            }
+        }
     }
 }
 
 impl IntoLaTeX for StyleNode {
     fn into_latex(self, cfg: &LaTeXConfig, ctx: &mut LaTeXContext) -> RcDoc<()> {
-        let _ = (cfg, ctx);
-        todo!()
+        match self.kind {
+            StyleKind::Plain => {
+                unimplemented!()
+            }
+            StyleKind::Emphasis => RcDoc::text("\\emph{")
+                .append(
+                    RcDoc::intersperse(self.children.into_iter().map(|x| x.into_latex(cfg, ctx)), RcDoc::softline_())
+                        .nest(1)
+                        .group(),
+                )
+                .append(RcDoc::text("}")),
+            StyleKind::Strong => RcDoc::text("\\textbf{")
+                .append(
+                    RcDoc::intersperse(self.children.into_iter().map(|x| x.into_latex(cfg, ctx)), RcDoc::softline_())
+                        .nest(1)
+                        .group(),
+                )
+                .append(RcDoc::text("}")),
+            StyleKind::ItalicBold => {
+                unimplemented!()
+            }
+            StyleKind::Underline => {
+                RcDoc::text("\\underline{")
+                    .append(
+                        RcDoc::intersperse(self.children.into_iter().map(|x| x.into_latex(cfg, ctx)), RcDoc::softline_())
+                            .nest(1)
+                            .group(),
+                    )
+                    .append(RcDoc::text("}")),
+            }
+            StyleKind::Undercover => {
+                unimplemented!()
+            }
+            StyleKind::Marking => {
+                unimplemented!()
+            }
+            StyleKind::Color(_, _, _, _) => {
+                unimplemented!()
+            }
+            StyleKind::Delete => {
+                unimplemented!()
+            }
+            StyleKind::Insert => {
+                unimplemented!()
+            }
+            StyleKind::Subscript => {
+                unimplemented!()
+            }
+            StyleKind::Superscript => {
+                unimplemented!()
+            }
+        }
     }
 }
