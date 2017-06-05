27c1350e206390a59acf0166aaa6f4ceb26819e0
Aster <galaster@foxmail.com>
2017-06-05 13:01:09 +0000

    🔮 Load template for lipsum

--- projects/lipsum-cn/src/hans/mod.rs
+++ projects/lipsum-cn/src/hans/mod.rs
@@ -1,19 +1,18 @@
-use handlebars::Handlebars;
+use std::collections::HashMap;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let mut reg = Handlebars::new();
-    // render without register
-    println!("{}", reg.render_template("Hello {{name}}", &json!({"name": "foo"}))?);
-
-    // register template using given name
-    reg.register_template_string("tpl_1", "Good afternoon, {{name}}")?;
-    println!("{}", reg.render("tpl_1", &json!({"name": "foo"}))?);
-
-    Ok(())
+fn 加载名言() -> HashMap<usize, &'static str> {
+    let mut h = HashMap::new();
+    for (i, s) in include_str!("名言.txt").lines().enumerate() {
+        let text = s.trim();
+        if !text.is_empty() {
+            h.insert(i, text);
+        }
+    }
+    return h;
 }
 
-pub static 名言模板: &'static =
-
-pub fn 生成名言() {
-    include_str!("名言.txt")
+#[test]
+fn test() {
+    let r = 加载名言();
+    println!("{:#?}", r);
 }
